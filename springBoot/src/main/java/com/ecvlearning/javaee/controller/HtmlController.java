4aae0567632b8f760c17748ab8151e4b7e817cc2
chandler <zhuqianca@gmail.com>
2017-11-01 20:03:47 -0400

    Spring data

--- springBoot/src/main/java/com/ecvlearning/javaee/controller/HtmlController.java
+++ springBoot/src/main/java/com/ecvlearning/javaee/controller/HtmlController.java
@@ -1,20 +1,37 @@
 package com.ecvlearning.javaee.controller;
 
 
+import com.ecvlearning.javaee.dao.UserRepo;
+import com.ecvlearning.javaee.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class HtmlController {
     Logger logger = LoggerFactory.getLogger(HtmlController.class);
+
+    @Autowired
+    UserRepo userRepo;
 
     @RequestMapping(value = "/page")
     public ModelAndView toPage(){
         logger.info("Html controller to page:");
         ModelAndView mav = new ModelAndView("pages/page.html");
         return mav;
+    }
+
+
+    @RequestMapping(value = "/userLt")
+    public @ResponseBody List<User> getUserLt(){
+        logger.info("Html controller userLt:");
+        List<User> list = this.userRepo.findAll();
+        return list;
     }
 }
---
+++ springBoot/src/main/java/com/ecvlearning/javaee/model/User.java
@@ -0,0 +1,60 @@
+package com.ecvlearning.javaee.model;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Entity
+public class User {
+
+    @Id
+    long id;
+    @Column(name = "user_name")
+    String userName;
+    String password;
+    String description;
+    Date birthday;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
